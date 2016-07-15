64adb5f460fc82f3f24744e6567bdf5aa9419cb6
Рауль Вахитов <mrgenga537@gmail.com>
2016-07-15 16:17:13 +0500

    Fix

--- src/main/java/cn/nukkit/level/particle/MobSpellParticle.java
+++ src/main/java/cn/nukkit/level/particle/MobSpellParticle.java
@@ -7,6 +7,18 @@ import cn.nukkit.math.Vector3;
  * Nukkit Project
  */
 public class MobSpellParticle extends GenericParticle{
+    public MobSpellParticle(Vector3 pos){
+        this(pos, 0, 0, 0, 0);
+    }
+    public MobSpellParticle(Vector3 pos, int r){
+        this(pos, r, 0, 0, 0);
+    }
+    public MobSpellParticle(Vector3 pos, int r, int g){
+        this(pos, r, g, 0, 0);
+    }
+    public MobSpellParticle(Vector3 pos, int r, int g, int b){
+        this(pos, r, g, b, 0);
+    }
     public MobSpellParticle(Vector3 pos, int r, int g, int b, int a){
         super(pos, Particle.TYPE_MOB_SPELL, ((a & 0xff) << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff));
     }
